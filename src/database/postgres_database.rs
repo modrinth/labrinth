6fa1606b44ab01844444f32de3aaf0a72b4fa1a9
AppleTheGolden <scotsbox@protonmail.com>
2020-07-18 23:08:51 +0200

    Update docker, documentation, gh actions...

--- src/database/postgres_database.rs
+++ src/database/postgres_database.rs
@@ -4,7 +4,7 @@ use sqlx::postgres::{PgPool, PgPoolOptions};
 pub async fn connect() -> Result<PgPool, sqlx::Error> {
     info!("Initializing database connection");
 
-    let database_url = dotenv::var("DATABASE_URL").expect("`MONGO_ADDR` not in .env");
+    let database_url = dotenv::var("DATABASE_URL").expect("`DATABASE_URL` not in .env");
     let pool = PgPoolOptions::new()
         .max_connections(20)
         .connect(&database_url)
