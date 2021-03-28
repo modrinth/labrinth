cf11474e8b91fa8740fcb804468e49b394d59bbc
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-03-28 22:30:45 +0300

    Remove indentation

--- src/routes/maven.rs
+++ src/routes/maven.rs
@@ -77,78 +77,78 @@ pub async fn maven_metadata(
 
     let user_option = get_user_from_headers(req.headers(), &**pool).await.ok();
 
-    if let Some(data) = mod_data {
-        let mut authorized = !data.status.is_hidden();
+    let data = if let Some(data) = mod_data {
+        data
+    } else {
+        return Ok(HttpResponse::NotFound().body(""));
+    };
 
-        if let Some(user) = user_option {
-            if !authorized {
-                if user.role.is_mod() {
-                    authorized = true;
-                } else {
-                    let user_id: database::models::ids::UserId = user.id.into();
+    let mut authorized = !data.status.is_hidden();
 
-                    let mod_exists = sqlx::query!(
-                        "SELECT EXISTS(SELECT 1 FROM team_members WHERE team_id = $1 AND user_id = $2)",
-                        data.inner.team_id as database::models::ids::TeamId,
-                        user_id as database::models::ids::UserId,
-                    )
-                    .fetch_one(&**pool)
-                    .await
-                    .map_err(|e| ApiError::DatabaseError(e.into()))?
-                    .exists;
+    if let Some(user) = user_option {
+        if !authorized {
+            if user.role.is_mod() {
+                authorized = true;
+            } else {
+                let user_id: database::models::ids::UserId = user.id.into();
 
-                    authorized = mod_exists.unwrap_or(false);
-                }
+                let mod_exists = sqlx::query!(
+                    "SELECT EXISTS(SELECT 1 FROM team_members WHERE team_id = $1 AND user_id = $2)",
+                    data.inner.team_id as database::models::ids::TeamId,
+                    user_id as database::models::ids::UserId,
+                )
+                .fetch_one(&**pool)
+                .await
+                .map_err(|e| ApiError::DatabaseError(e.into()))?
+                .exists;
+
+                authorized = mod_exists.unwrap_or(false);
             }
         }
-
-        if authorized {
-            let version_names = sqlx::query!(
-                "
-                SELECT version_number, release_channels.channel channel
-                FROM versions
-                LEFT JOIN release_channels ON release_channels.id = versions.release_channel
-                WHERE mod_id = $1
-                ",
-                data.inner.id as database::models::ids::ModId
-            )
-            .fetch_all(&**pool)
-            .await
-            .map_err(|e| ApiError::DatabaseError(e.into()))?;
-
-            let respdata = Metadata {
-                group_id: "maven.modrinth".to_string(),
-                artifact_id: string,
-                versioning: Versioning {
-                    latest: version_names
-                        .last()
-                        .map_or("release", |x| &x.version_number)
-                        .to_string(),
-                    release: version_names
-                        .iter()
-                        .rfind(|x| x.channel == "release")
-                        .map_or("", |x| &x.version_number)
-                        .to_string(),
-                    versions: Versions {
-                        versions: version_names
-                            .iter()
-                            .map(|x| x.version_number.clone())
-                            .collect::<Vec<_>>(),
-                    },
-                    last_updated: data.inner.updated.format("%Y%m%d%H%M%S").to_string(),
-                },
-            };
-
-            // return Ok(HttpResponse::Ok().content_type("text/xml").body(response));
-            Ok(HttpResponse::Ok()
-                .content_type("text/xml")
-                .body(yaserde::ser::to_string(&respdata).map_err(|e| ApiError::XmlError(e))?))
-        } else {
-            Ok(HttpResponse::NotFound().body(""))
-        }
-    } else {
-        Ok(HttpResponse::NotFound().body(""))
     }
+
+    if !authorized {
+        return Ok(HttpResponse::NotFound().body(""));
+    }
+    let version_names = sqlx::query!(
+        "
+            SELECT version_number, release_channels.channel channel
+            FROM versions
+            LEFT JOIN release_channels ON release_channels.id = versions.release_channel
+            WHERE mod_id = $1
+            ",
+        data.inner.id as database::models::ids::ModId
+    )
+    .fetch_all(&**pool)
+    .await
+    .map_err(|e| ApiError::DatabaseError(e.into()))?;
+
+    let respdata = Metadata {
+        group_id: "maven.modrinth".to_string(),
+        artifact_id: string,
+        versioning: Versioning {
+            latest: version_names
+                .last()
+                .map_or("release", |x| &x.version_number)
+                .to_string(),
+            release: version_names
+                .iter()
+                .rfind(|x| x.channel == "release")
+                .map_or("", |x| &x.version_number)
+                .to_string(),
+            versions: Versions {
+                versions: version_names
+                    .iter()
+                    .map(|x| x.version_number.clone())
+                    .collect::<Vec<_>>(),
+            },
+            last_updated: data.inner.updated.format("%Y%m%d%H%M%S").to_string(),
+        },
+    };
+
+    Ok(HttpResponse::Ok()
+        .content_type("text/xml")
+        .body(yaserde::ser::to_string(&respdata).map_err(|e| ApiError::XmlError(e))?))
 }
 
 #[get("maven/modrinth/{id}/{versionnum}/{file}")]
@@ -179,83 +179,92 @@ pub async fn version_file(
 
     let user_option = get_user_from_headers(req.headers(), &**pool).await.ok();
 
-    if let Some(data) = mod_data {
-        let mut authorized = !data.status.is_hidden();
+    let data = if let Some(data) = mod_data {
+        data
+    } else {
+        return Ok(HttpResponse::NotFound().body(""));
+    };
 
-        if let Some(user) = user_option {
-            if !authorized {
-                if user.role.is_mod() {
-                    authorized = true;
-                } else {
-                    let user_id: database::models::ids::UserId = user.id.into();
+    let mut authorized = !data.status.is_hidden();
 
-                    let mod_exists = sqlx::query!(
-                        "SELECT EXISTS(SELECT 1 FROM team_members WHERE team_id = $1 AND user_id = $2)",
-                        data.inner.team_id as database::models::ids::TeamId,
-                        user_id as database::models::ids::UserId,
-                    )
-                    .fetch_one(&**pool)
-                    .await
-                    .map_err(|e| ApiError::DatabaseError(e.into()))?
-                    .exists;
+    if let Some(user) = user_option {
+        if !authorized {
+            if user.role.is_mod() {
+                authorized = true;
+            } else {
+                let user_id: database::models::ids::UserId = user.id.into();
 
-                    authorized = mod_exists.unwrap_or(false);
-                }
-            }
-        }
-
-        if authorized {
-            let vid_option = sqlx::query!(
-                "SELECT id FROM versions WHERE mod_id = $1 AND version_number = $2",
-                data.inner.id as database::models::ids::ModId,
-                vnum
-            )
-            .fetch_optional(&**pool)
-            .await
-            .map_err(|e| ApiError::DatabaseError(e.into()))?;
-
-            if let Some(vid) = vid_option {
-                let version_option = database::models::Version::get_full(
-                    database::models::ids::VersionId(vid.id),
-                    &**pool,
+                let mod_exists = sqlx::query!(
+                    "SELECT EXISTS(SELECT 1 FROM team_members WHERE team_id = $1 AND user_id = $2)",
+                    data.inner.team_id as database::models::ids::TeamId,
+                    user_id as database::models::ids::UserId,
                 )
+                .fetch_one(&**pool)
                 .await
-                .map_err(|e| ApiError::DatabaseError(e.into()))?;
+                .map_err(|e| ApiError::DatabaseError(e.into()))?
+                .exists;
 
-                if let Some(version) = version_option {
-                    if file == format!("{}-{}.pom", &string, &version.version_number) {
-                        let respdata = MavenPom {
-                            schema_location: "http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd".to_string(),
-                            xsi: "http://www.w3.org/2001/XMLSchema-instance".to_string(),
-                            model_version: "4.0.0".to_string(),
-                            group_id: "maven.modrinth".to_string(),
-                            artifact_id: string,
-                            version: version.version_number,
-                            name: data.inner.title,
-                            description: data.inner.description,
-                        };
-                        return Ok(HttpResponse::Ok().content_type("text/xml").body(
-                            yaserde::ser::to_string(&respdata)
-                                .map_err(|e| ApiError::XmlError(e))?,
-                        ));
-                    } else {
-                        if let Some(selected_file) =
-                            version.files.iter().find(|x| x.filename == file)
-                        {
-                            return Ok(HttpResponse::TemporaryRedirect()
-                                .header("Location", &*selected_file.url)
-                                .body(""));
-                        } else if file == format!("{}-{}.jar", &string, &version.version_number) {
-                            if let Some(selected_file) = version.files.iter().last() {
-                                return Ok(HttpResponse::TemporaryRedirect()
-                                    .header("Location", &*selected_file.url)
-                                    .body(""));
-                            }
-                        };
-                    }
-                }
+                authorized = mod_exists.unwrap_or(false);
             }
         }
+    }
+
+    if !authorized {
+        return Ok(HttpResponse::NotFound().body(""));
+    }
+
+    let vid = if let Some(vid) = sqlx::query!(
+        "SELECT id FROM versions WHERE mod_id = $1 AND version_number = $2",
+        data.inner.id as database::models::ids::ModId,
+        vnum
+    )
+    .fetch_optional(&**pool)
+    .await
+    .map_err(|e| ApiError::DatabaseError(e.into()))?
+    {
+        vid
+    } else {
+        return Ok(HttpResponse::NotFound().body(""));
+    };
+
+    let version = if let Some(version) =
+        database::models::Version::get_full(database::models::ids::VersionId(vid.id), &**pool)
+            .await
+            .map_err(|e| ApiError::DatabaseError(e.into()))?
+    {
+        version
+    } else {
+        return Ok(HttpResponse::NotFound().body(""));
+    };
+
+    if file == format!("{}-{}.pom", &string, &version.version_number) {
+        let respdata = MavenPom {
+            schema_location:
+                "http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd"
+                    .to_string(),
+            xsi: "http://www.w3.org/2001/XMLSchema-instance".to_string(),
+            model_version: "4.0.0".to_string(),
+            group_id: "maven.modrinth".to_string(),
+            artifact_id: string,
+            version: version.version_number,
+            name: data.inner.title,
+            description: data.inner.description,
+        };
+        return Ok(HttpResponse::Ok()
+            .content_type("text/xml")
+            .body(yaserde::ser::to_string(&respdata).map_err(|e| ApiError::XmlError(e))?));
+    } else {
+        if let Some(selected_file) = version.files.iter().find(|x| x.filename == file) {
+            return Ok(HttpResponse::TemporaryRedirect()
+                .header("Location", &*selected_file.url)
+                .body(""));
+        } else if file == format!("{}-{}.jar", &string, &version.version_number) {
+            if let Some(selected_file) = version.files.iter().last() {
+                return Ok(HttpResponse::TemporaryRedirect()
+                    .header("Location", &*selected_file.url)
+                    .body(""));
+            }
+        };
     }
 
     Ok(HttpResponse::NotFound().body(""))
