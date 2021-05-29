8abccd9442579a62653013a246825a418e5dea49
Redblueflame <contact@redblueflame.com>
2021-05-29 14:29:21 +0200

    Fixed clippy error

--- src/database/cache/mod.rs
+++ src/database/cache/mod.rs
@@ -16,10 +16,7 @@ macro_rules! generate_cache {
             pub async fn $getter_name<'a>(id: $id) -> Option<$val> {
                 let mut cache = $cache_name.lock().await;
                 info!("Querying cache {} for {}", stringify!($name), id);
-                match Cached::cache_get(&mut *cache, &id) {
-                    Some(e) => Some(e.clone()),
-                    None => None,
-                }
+                Cached::cache_get(&mut *cache, &id).map(|e| e.clone())
             }
             pub async fn $setter_name<'a>(id: $id, val: &$val) {
                 let mut cache = $cache_name.lock().await;
