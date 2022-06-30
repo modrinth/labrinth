654583e88069afcd327b58bede73d3c958017356
Emma Cypress Pointer-Null <emmaffle@modrinth.com>
2022-06-29 20:18:32 -0400

    Add even more validators
    I was gonna add shaderpacks too, but those have no standard metadata file at all.

--- src/validate/resourcepack.rs
+++ src/validate/resourcepack.rs
@@ -4,19 +4,19 @@ use crate::validate::{
 use std::io::Cursor;
 use zip::ZipArchive;
 
-pub struct ResourcePackValidator;
+pub struct JavaPackValidator;
 
-impl super::Validator for ResourcePackValidator {
+impl super::Validator for JavaPackValidator {
     fn get_file_extensions(&self) -> &[&str] {
         &["zip"]
     }
 
     fn get_project_types(&self) -> &[&str] {
-        &["resourcepack"]
+        &["resourcepack", "datapack"]
     }
 
     fn get_supported_loaders(&self) -> &[&str] {
-        &["minecraft"]
+        &["java"]
     }
 
     fn get_supported_game_versions(&self) -> SupportedGameVersions {
@@ -29,7 +29,40 @@ impl super::Validator for ResourcePackValidator {
     ) -> Result<ValidationResult, ValidationError> {
         archive.by_name("pack.mcmeta").map_err(|_| {
             ValidationError::InvalidInput(
-                "No pack.mcmeta present for resourcepack file.".into(),
+                "No pack.mcmeta present for pack file.".into(),
+            )
+        })?;
+
+        Ok(ValidationResult::Pass)
+    }
+}
+
+pub struct BedrockPackValidator;
+
+impl super::Validator for BedrockPackValidator {
+    fn get_file_extensions(&self) -> &[&str] {
+        &["mcpack"]
+    }
+
+    fn get_project_types(&self) -> &[&str] {
+        &["resourcepack", "datapack"]
+    }
+
+    fn get_supported_loaders(&self) -> &[&str] {
+        &["bedrock"]
+    }
+
+    fn get_supported_game_versions(&self) -> SupportedGameVersions {
+        SupportedGameVersions::All
+    }
+
+    fn validate(
+        &self,
+        archive: &mut ZipArchive<Cursor<bytes::Bytes>>,
+    ) -> Result<ValidationResult, ValidationError> {
+        archive.by_name("manifest.json").map_err(|_| {
+            ValidationError::InvalidInput(
+                "No manifest.json present for pack file.".into(),
             )
         })?;
 
