d067574e5a0cba218a7d7e2c15574cdcb22fedee
Emma Cypress Pointer-Null <emmaffle@modrinth.com>
2022-10-03 18:12:15 -0400

    Make auto the default config value for locale and theme

--- migrations/20220822175311_user-settings.sql
+++ migrations/20220822175311_user-settings.sql
@@ -1,8 +1,8 @@
 CREATE TABLE user_settings (
     user_id bigint NOT NULL PRIMARY KEY UNIQUE,
     public_github boolean NOT NULL DEFAULT TRUE,
-    theme varchar(32) NOT NULL DEFAULT 'system',
-    locale varchar(32) NOT NULL DEFAULT 'en-US'
+    theme varchar(32) NOT NULL DEFAULT 'auto',
+    locale varchar(32) NOT NULL DEFAULT 'auto'
 );
 
 INSERT INTO user_settings (user_id)
