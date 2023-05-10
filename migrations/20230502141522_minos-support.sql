0d3af049c4a8a1f0cf4961ffd717922972156e03
Wyatt <wyatt@modrinth.com>
2023-05-10 12:03:26 -0700

    lowered kratos_id size

--- migrations/20230502141522_minos-support.sql
+++ migrations/20230502141522_minos-support.sql
@@ -6,7 +6,7 @@ ALTER TABLE banned_users DROP COLUMN github_id;
 
 -- Initialize kratos_id for existing users, starting as unique 'uninitialized_<user id>'
 -- After account porting, this will be set to the kratos_id and NO record should have 'uninitialized_' prefix
-ALTER TABLE users ADD COLUMN kratos_id varchar(255);
+ALTER TABLE users ADD COLUMN kratos_id varchar(40);
 UPDATE users SET kratos_id = 'uninitialized_' || id;
 ALTER TABLE users ALTER COLUMN kratos_id SET NOT NULL;
 ALTER TABLE users ADD CONSTRAINT kratos_id_unique UNIQUE (kratos_id);
