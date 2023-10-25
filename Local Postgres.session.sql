196b3533198c3972f11b2b3ba5374193009037c7
OmegaJak <jak.kruger@gmail.com>
2023-10-24 21:37:26 -0500

    Remove file that shouldn't have been committed

--- Local Postgres.session.sql
+++
@@ -1,8 +0,0 @@
-DROP INDEX events_targets;
-DROP INDEX events_triggerers;
-DROP TABLE events;
-DROP TYPE dynamic_id;
-DROP TYPE event_type;
-DROP TYPE id_type;
-DELETE FROM _sqlx_migrations
-WHERE version = 20231023212957;
