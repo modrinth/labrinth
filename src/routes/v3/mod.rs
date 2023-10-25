f5f7b83d7aee968ce041550d55d32a24e7b5f0b7
OmegaJak <jak.kruger@gmail.com>
2023-10-25 12:38:12 -0500

    formatting

--- src/routes/v3/mod.rs
+++ src/routes/v3/mod.rs
@@ -1,13 +1,12 @@
-pub mod follow;
-pub mod organizations;
-pub mod users;
-
 pub use super::ApiError;
 use crate::{auth::oauth, util::cors::default_cors};
 use actix_web::{web, HttpResponse};
 use serde_json::json;
 
+pub mod follow;
 pub mod oauth_clients;
+pub mod organizations;
+pub mod users;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
