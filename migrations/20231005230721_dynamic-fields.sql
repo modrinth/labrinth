f765ce45382f908bc6d5d5ad2de8211e5a164cb2
Jai A <jaiagr+gpg@pm.me>
2024-01-03 19:45:14 -0500

    Update migrations

--- migrations/20231005230721_dynamic-fields.sql
+++ migrations/20231005230721_dynamic-fields.sql
@@ -99,8 +99,12 @@ SELECT id, 2, version, created, json_build_object('type', type, 'major', major) 
 INSERT INTO loader_fields (field, field_type, enum_type, optional, min_val) VALUES('game_versions', 'array_enum', 2, false, 0);
 INSERT INTO loader_fields_loaders (loader_id, loader_field_id) SELECT l.id, lf.id FROM loaders l CROSS JOIN loader_fields lf  WHERE lf.field = 'game_versions' AND l.loader = ANY( ARRAY['forge', 'fabric', 'quilt', 'modloader','rift','liteloader', 'neoforge']);
 
-INSERT INTO version_fields(version_id, field_id, enum_value) 
-SELECT gvv.joining_version_id, lf.id, lfev.id 
+-- remove dangling game versions
+DELETE FROM game_versions_versions
+WHERE joining_version_id NOT IN (SELECT id FROM versions);
+
+INSERT INTO version_fields(version_id, field_id, enum_value)
+SELECT gvv.joining_version_id, lf.id, lfev.id
 FROM game_versions_versions gvv INNER JOIN loader_field_enum_values lfev ON gvv.game_version_id = lfev.original_id
 CROSS JOIN loader_fields lf
 WHERE lf.field = 'game_versions' AND lfev.enum_id = 2;
