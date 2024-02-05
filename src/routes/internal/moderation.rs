dfbbcca61e431ffce075a0849ba9ebdaf578689a
Jai A <jaiagr+gpg@pm.me>
2024-02-04 21:49:47 -0700

    finish

--- src/routes/internal/moderation.rs
+++ src/routes/internal/moderation.rs
@@ -1,12 +1,17 @@
 use super::ApiError;
 use crate::database;
 use crate::database::redis::RedisPool;
+use crate::models::ids::random_base62;
 use crate::models::projects::ProjectStatus;
+use crate::queue::moderation::{
+    ApprovalType, IdentifiedFile, MissingMetadata, ModerationMessage, ModerationMessages,
+};
 use crate::queue::session::AuthQueue;
 use crate::{auth::check_is_moderator_from_headers, models::pats::Scopes};
 use actix_web::{web, HttpRequest, HttpResponse};
 use serde::Deserialize;
 use sqlx::PgPool;
+use std::collections::HashMap;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.route("moderation/projects", web::get().to(get_projects));
@@ -66,15 +71,12 @@ pub async fn get_projects(
     Ok(HttpResponse::Ok().json(projects))
 }
 
-// TODO: get judgements route
-// TODO: submit judgements route
-
 pub async fn get_project_meta(
     req: HttpRequest,
     pool: web::Data<PgPool>,
     redis: web::Data<RedisPool>,
     session_queue: web::Data<AuthQueue>,
-    project_id: web::Path<String>,
+    info: web::Path<(String,)>,
 ) -> Result<HttpResponse, ApiError> {
     check_is_moderator_from_headers(
         &req,
@@ -85,10 +87,65 @@ pub async fn get_project_meta(
     )
     .await?;
 
-    // get project files metadata files
-    // return hashmap of metadata and version ids
+    let project_id = info.into_inner().0;
+    let project = database::models::Project::get(&project_id, &**pool, &redis).await?;
 
-    Ok(HttpResponse::Ok().finish())
+    if let Some(project) = project {
+        let rows = sqlx::query!(
+            "
+            SELECT v.id version_id, f.metadata
+            FROM versions v
+            INNER JOIN files f ON f.version_id = v.id
+            WHERE v.mod_id = $1
+            ",
+            project.inner.id.0
+        )
+        .fetch_all(&**pool)
+        .await?;
+
+        let mut merged = MissingMetadata {
+            identified: HashMap::new(),
+            flame_files: HashMap::new(),
+            unknown_files: HashMap::new(),
+        };
+
+        for row in rows {
+            if let Some(metadata) = row
+                .metadata
+                .and_then(|x| serde_json::from_value::<MissingMetadata>(x).ok())
+            {
+                merged.identified.extend(metadata.identified);
+                merged.flame_files.extend(metadata.flame_files);
+                merged.unknown_files.extend(metadata.unknown_files);
+            }
+        }
+
+        Ok(HttpResponse::Ok().json(merged))
+    } else {
+        Err(ApiError::NotFound)
+    }
+}
+
+#[derive(Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum Judgement {
+    Flame {
+        id: i32,
+        status: ApprovalType,
+        link: String,
+        title: String,
+    },
+    Unknown {
+        status: ApprovalType,
+        proof: Option<String>,
+        link: Option<String>,
+        title: Option<String>,
+    },
+}
+
+#[derive(Deserialize)]
+pub struct Judgements {
+    pub judgements: HashMap<String, Judgement>,
 }
 
 pub async fn set_project_meta(
@@ -96,7 +153,8 @@ pub async fn set_project_meta(
     pool: web::Data<PgPool>,
     redis: web::Data<RedisPool>,
     session_queue: web::Data<AuthQueue>,
-    project_id: web::Path<String>,
+    info: web::Path<(String,)>,
+    judgements: web::Json<Judgements>,
 ) -> Result<HttpResponse, ApiError> {
     check_is_moderator_from_headers(
         &req,
@@ -107,10 +165,161 @@ pub async fn set_project_meta(
     )
     .await?;
 
-    // return mod message based on judgements
-    // in separate task:
-    // register judgements in DB
-    // update flame projects and put in db
+    let project_id = info.into_inner().0;
+    let project = database::models::Project::get(&project_id, &**pool, &redis).await?;
 
-    Ok(HttpResponse::Ok().finish())
+    if let Some(project) = project {
+        let mut moderator_messages = ModerationMessages {
+            messages: vec![],
+            version_specific: HashMap::new(),
+        };
+
+        let rows = sqlx::query!(
+            "
+            SELECT v.id version_id, v.version_number, f.metadata
+            FROM versions v
+            INNER JOIN files f ON f.version_id = v.id
+            WHERE v.mod_id = $1
+            ",
+            project.inner.id.0
+        )
+        .fetch_all(&**pool)
+        .await?;
+
+        for row in rows {
+            if let Some(mut metadata) = row
+                .metadata
+                .and_then(|x| serde_json::from_value::<MissingMetadata>(x).ok())
+            {
+                for (sha1, file_name) in metadata.unknown_files {
+                    if let Some(val) = judgements.judgements.get(&sha1) {
+                        match val {
+                            Judgement::Flame { status, .. } => metadata.identified.insert(
+                                sha1,
+                                IdentifiedFile {
+                                    file_name,
+                                    status: *status,
+                                },
+                            ),
+                            Judgement::Unknown { status, .. } => metadata.identified.insert(
+                                sha1,
+                                IdentifiedFile {
+                                    file_name,
+                                    status: *status,
+                                },
+                            ),
+                        };
+                    }
+                }
+
+                for (sha1, file_name) in metadata.flame_files {
+                    if let Some(val) = judgements.judgements.get(&sha1) {
+                        match val {
+                            Judgement::Flame { status, .. } => metadata.identified.insert(
+                                sha1,
+                                IdentifiedFile {
+                                    file_name: file_name.file_name,
+                                    status: *status,
+                                },
+                            ),
+                            Judgement::Unknown { status, .. } => metadata.identified.insert(
+                                sha1,
+                                IdentifiedFile {
+                                    file_name: file_name.file_name,
+                                    status: *status,
+                                },
+                            ),
+                        };
+                    }
+                }
+
+                moderator_messages.version_specific.insert(
+                    row.version_number,
+                    vec![ModerationMessage::PackFilesNotAllowed {
+                        files: metadata.identified,
+                        incomplete: false,
+                    }],
+                );
+            }
+        }
+
+        let mut transaction = pool.begin().await?;
+
+        let mut ids = Vec::new();
+        let mut titles = Vec::new();
+        let mut statuses = Vec::new();
+        let mut links = Vec::new();
+        let mut proofs = Vec::new();
+        let mut flame_ids = Vec::new();
+
+        let mut file_hashes = Vec::new();
+
+        for (hash, judgement) in judgements.0.judgements {
+            let id = random_base62(8);
+
+            let (title, status, link, proof, flame_id) = match judgement {
+                Judgement::Flame {
+                    id,
+                    status,
+                    link,
+                    title,
+                } => (
+                    Some(title),
+                    status,
+                    Some(link),
+                    Some("See Flame page/license for permission".to_string()),
+                    Some(id),
+                ),
+                Judgement::Unknown {
+                    status,
+                    proof,
+                    link,
+                    title,
+                } => (title, status, link, proof, None),
+            };
+
+            ids.push(id as i64);
+            titles.push(title);
+            statuses.push(status.as_str());
+            links.push(link);
+            proofs.push(proof);
+            flame_ids.push(flame_id);
+            file_hashes.push(hash);
+        }
+
+        sqlx::query(
+            "
+            INSERT INTO moderation_external_licenses (id, title, status, link, proof, flame_project_id)
+            SELECT * FROM UNNEST ($1::bigint[], $2::varchar[], $3::varchar[], $4::varchar[], $5::varchar[], $6::integer[])
+            "
+        )
+            .bind(&ids[..])
+            .bind(&titles[..])
+            .bind(&statuses[..])
+            .bind(&links[..])
+            .bind(&proofs[..])
+            .bind(&flame_ids[..])
+            .execute(&mut *transaction)
+            .await?;
+
+        sqlx::query(
+            "
+                INSERT INTO moderation_external_files (sha1, external_license_id)
+                SELECT * FROM UNNEST ($1::bytea[], $2::bigint[])
+                ",
+        )
+        .bind(&file_hashes[..])
+        .bind(&ids[..])
+        .execute(&mut *transaction)
+        .await?;
+
+        transaction.commit().await?;
+
+        Ok(HttpResponse::Ok().json(serde_json::json!({
+            "approvable": moderator_messages.approvable(),
+            "message": moderator_messages.markdown(false),
+        })))
+    } else {
+        Err(ApiError::NotFound)
+    }
 }
