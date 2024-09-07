e57a8541dbc37df1ebb55cb1c8d7eda3d561d3f0
Jai A <jaiagr+gpg@pm.me>
2024-09-06 23:32:12 -0700

    Slack webhooks

--- src/queue/moderation.rs
+++ src/queue/moderation.rs
@@ -660,15 +660,15 @@ impl AutomatedModerationQueue {
                                         .insert_many(members.into_iter().map(|x| x.user_id).collect(), &mut transaction, &redis)
                                         .await?;
 
-                                    if let Ok(webhook_url) = dotenvy::var("MODERATION_DISCORD_WEBHOOK") {
-                                        crate::util::webhook::send_discord_webhook(
+                                    if let Ok(webhook_url) = dotenvy::var("MODERATION_SLACK_WEBHOOK") {
+                                        crate::util::webhook::send_slack_webhook(
                                             project.inner.id.into(),
                                             &pool,
                                             &redis,
                                             webhook_url,
                                             Some(
                                                 format!(
-                                                    "**[AutoMod]({}/user/AutoMod)** changed project status from **{}** to **Rejected**",
+                                                    "*<{}/user/AutoMod|AutoMod>* changed project status from *{}* to *Rejected*",
                                                     dotenvy::var("SITE_URL")?,
                                                     &project.inner.status.as_friendly_str(),
                                                 )
